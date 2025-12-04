119ed2a4ca639159262ea137afa035c8d4f1621e
VikasThakur <vikasthakur1723@gmail.com>
2025-12-04 20:36:19 +0530

    add webhook sync

--- src/app/api/users/webhook/route.ts
+++ src/app/api/users/webhook/route.ts
@@ -4,6 +4,7 @@ import { NextResponse } from 'next/server';
 import { WebhookEvent } from '@clerk/nextjs/server';
 import { db } from '@/db';
 import { users } from '@/db/schema';
+import { eq } from 'drizzle-orm';
 
 
 export async function POST(req: Request) {
@@ -27,29 +28,50 @@ export async function POST(req: Request) {
     const payload = await req.json();
     const body = JSON.stringify(payload);
 
-    let event: WebhookEvent;
+    let evt: WebhookEvent;
 
     try{
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        event = wh.verify(body,{'svix-id': svix_id, 'svix-timestamp': svix_timestamp, 'svix-signature': svix_signature}) as WebhookEvent;
+       
+        evt = wh.verify(body,{'svix-id': svix_id, 'svix-timestamp': svix_timestamp, 'svix-signature': svix_signature}) as WebhookEvent;
     }catch(err){
         return NextResponse.json({ message: `Invalid signature : ${err}` }, { status: 400 });
     }
 
 
-    const eventType = event.type;
+    const eventType = evt.type;
     console.log(`Received event type: ${eventType}`);
 
-    // if(eventType === 'user.deleted'){
-    //      const { data } = event;
-    //      const d: any = data;
-    //      const name = d?.firstName ? `${d.firstName} ${d.lastName ?? ''}` : d?.emailAddresses?.[0]?.emailAddress ?? d?.email ?? 'Unknown';
-    //   // Handle user deletion logic here
-    //   await db.insert(users).values({
-    //     clerkId: d.id,
-    //     name: name,
-    //     imageUrl: d.imageUrl,
-    //   })
-    // }
+    if(eventType === 'user.created'){
+         const { data } = evt;
+
+         // Handle user deletion logic here
+      await db.insert(users).values({
+        clerkId: data.id,
+        name: `${data.first_name} ${data.last_name}`,
+        imageUrl: data.image_url,
+      })
+    }
+
+    if(eventType === 'user.deleted'){
+      const { data } = evt;
+
+      if(!data.id){
+        return NextResponse.json({ message: 'User ID is missing in the payload' }, { status: 400 });
+      }
+      // Handle user deletion logic here
+      await db.delete(users).where(eq(users.clerkId, data.id));
+    }
+
+    if(eventType === 'user.updated'){
+      const { data } = evt;
+      if(!data.id){
+        return NextResponse.json({ message: 'User ID is missing in the payload' }, { status: 400 });
+      }
+
+      await db.update(users).set({
+        name: `${data.first_name} ${data.last_name}`,
+        imageUrl: data.image_url,
+      }).where(eq(users.clerkId, data.id));
+    }
     return NextResponse.json({ message: 'Webhook received successfully' }, { status: 200 });
 }
