b370338a64ffb17264746933470faeb80cdce398
VikasThakur <vikasthakur1723@gmail.com>
2025-12-08 21:07:29 +0530

    add video schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,5 +1,5 @@
 
-
+import { relations } from "drizzle-orm";
 import { pgTable, text, timestamp, uuid, uniqueIndex  } from "drizzle-orm/pg-core";
 
 
@@ -13,6 +13,10 @@ export const users = pgTable("users", {
 
 }, (t) => [uniqueIndex("idx_users_clerk_id").on(t.clerkId)]);
 
+export const userRelations = relations(users, ({many}) =>({
+    videos:many(videos),
+}))
+
 
 export const categories = pgTable("categories", {
     id: uuid("id").primaryKey().defaultRandom(),
@@ -21,3 +25,24 @@ export const categories = pgTable("categories", {
     createdAt: timestamp("created_at").defaultNow().notNull(),
     updatedAt: timestamp("updated_at").defaultNow().notNull(),
 }, (t) => [uniqueIndex("idx_categories_name").on(t.name)]);
+
+export const videos = pgTable("videos", {
+    id: uuid("id").primaryKey().defaultRandom(),
+    title: text("title").notNull(),
+    description: text("description"),
+    userId: uuid("user_id").references(() => users.id, {
+        onDelete: "cascade",
+    }).notNull(),
+    categoryId: uuid("category_id").references(() => categories.id, {
+        onDelete:"set null"
+    }),
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updated_at").defaultNow().notNull(),
+});
+
+export const videoRelations = relations(videos, ({one}) => ({
+   user: one(users, {
+    fields: [videos.userId],
+    references:[users.id],
+   })
+}));
