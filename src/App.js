f4220cd0b4ea206d5630f001630fc708debb67c2
VikasThakur <vikasthakur1723@gmail.com>
2026-01-13 19:22:12 +0530

    declear the all components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import './App.css';
-
+import { BrowserRouter , Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+import { Navbar, SearchFeed, ChannelDetail, VideoDetail, Feed } from "./components";
 function App() {
   return (
-    <div className="App">
-      <h1>YouTube Clone</h1>
-    </div>
+    <BrowserRouter>
+       <Box sx={{backgroundColor: "#000"}}>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Feed />} />
+          <Route path="/video/:id" element={<VideoDetail />} />
+          <Route path="/channel/:id" element={<ChannelDetail />} />
+          <Route path="/search/:searchTerm" element={<SearchFeed />} />
+        </Routes>
+       </Box>
+    </BrowserRouter>
   );
 }
 
