b4ef131f20a3f9e76f37d42f76f32c0ef3da733e
VikasThakur <vikasthakur1723@gmail.com>
2026-01-16 23:00:37 +0530

    implement search functionality in SearchFeed component with API data fetching

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
-
+import React,{ useState, useEffect } from 'react'
+import { Box, Typography } from '@mui/material'
+import { useParams } from 'react-router-dom'
+import Videos from './Videos'
+import { fetchApi } from '../utils/fetchApi'
 const SearchFeed = () => {
+  const [videos, setVideos] = useState([]);
+  const { searchTerm } = useParams();
+
+  useEffect(() => {
+    fetchApi(`search?part=snippet&q=${searchTerm}`)
+      .then((data) => setVideos(data?.items));
+  }, [searchTerm]);
+
   return (
-    <div>
-      
-    </div>
+    <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
+      <Typography variant="h4" mb={2} ml={4} sx={{ color: 'white' }}>
+        Search Results for : <span style={{ color: '#f80808', fontWeight: 'bold' }}>{searchTerm}</span>
+      </Typography>
+      <Videos video={videos} />
+    </Box>
   )
 }
 
